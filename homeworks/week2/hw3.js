9cd470b72cc604302064d9b73e1b8562ff9c8689
SeaCow <jazztyy@gmail.com>
2020-06-20 00:40:21 +0800

    hw(1,3,4,5)finshed

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  
+  let ans = []
+  let arr = str.split('')
+  for(let i = arr.length; i >= 0; i--){
+      ans.push(arr[i])
+  }
+  return ans.join('')
 }
 
 reverse('hello');
--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,9 @@
 function printFactor(n) {
-  
+  for (let i = 1; i <= n; i++){
+      if(!(n % i)){
+          console.log(i);
+      }
+  }
 }
 
 printFactor(10);
